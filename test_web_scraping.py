c46901850fce051f388a36f3cb43820a08062912
lizajunaidi <lizajunaidi@gmail.com>
2026-03-07 14:38:22 -0800

    selenium webiste title test

--- test_web_scraping.py
+++ test_web_scraping.py
@@ -5,4 +5,4 @@ url = "https://libcal.library.ubc.ca/reserve/woodward_group_study"
 response = requests.get(url)
 
 print(response.status_code)   
-print(response.text)         
+print(response.text)          
