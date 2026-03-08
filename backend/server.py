c61c77bcc30ac44b07f45a21db5af963499e4b8b
Mahtab <zehtabma@gmail.com>
2026-03-08 00:36:50 -0800

    update server + rename backend file

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,30 @@
 from flask import Flask, jsonify, request
-from backend.selenium import searchForClasses
-
+from flask_cors import CORS
+from scraper_tool import searchForClasses
 
 app = Flask(__name__)
+CORS(app)
+
 site_urls = [
     "https://libcal.library.ubc.ca/reserve/woodward_group_study",
 ]
 
-@app.route("/study_rooms", methods=['GET']) # type: ignore
+@app.route("/study_rooms", methods=["POST"])
 def handle_data():
-    date, start_time, end_time = request.get_json()
+    data = request.get_json()
+
+    date = data["date"]
+    start_time = data["start_time"]
+    end_time = data["end_time"]
+
     result = []
+
     for s in site_urls:
-        searchForClasses(s, date, start_time, end_time)
-    
+        rooms = searchForClasses(s, date, start_time, end_time)
+        result.extend(rooms)
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
