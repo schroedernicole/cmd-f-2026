00a0a266a2339441b12ec1bbcb76b0990a5fa45c
nschro01 <nschro01@students.cs.ubc.ca>
2026-03-07 22:15:16 -0800

    decluttering(delete unused files)

--- backend/selenium_test.py
+++
@@ -1,6 +0,0 @@
-
-from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get("https://libcal.library.ubc.ca/spaces")
-title = driver.title
-print(title)
--- backend/test_web_scraping.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://libcal.library.ubc.ca/reserve/woodward_group_study"
-
-response = requests.get(url)
-
-print(response.status_code)   
-print(response.text)          
